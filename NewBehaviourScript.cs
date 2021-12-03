2d0ad9afb035a4d1dc29ee4f73b4dfa98b1e0e6c
BenToronto <83716543+BenToronto@users.noreply.github.com>
2021-12-03 14:48:39 -0500

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        j
+		i;i];d9\[
+		
+		uj == if(hghg)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ h.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+// A behaviour that is attached to a playable
+public class h : PlayableBehaviour
+{
+    // Called when the owning graph starts playing
+    public override void OnGraphStart(Playable playable)
+    {
+        dt
+            dt; ;
+        h/ReadOnlyCollectionBase switch das
+            fill-float f[]=OnBehaviourPause
+            fill-foreach get get"json";
+        PrepareFrame;
+    }
+
+    // Called when the owning graph stops playing
+    public override void OnGraphStop(Playable playable)
+    {
+        OnBehaviourPlay(KeyValuePair<Vector3>)
+            fill-byte fast-Hashtable<bool nun>
+    }
+
+    // Called when the state of the playable is set to Play
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    {
+        ToString0 - HashSet<float>;
+        Dictionary/
+    }
+
+    // Called when the state of the playable is set to Paused
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+        float x;
+        float y;
+        float z;
+    }
+
+    // Called each frame while the state is set to Play
+    public override void PrepareFrame(Playable playable, FrameData info)
+    {
+        delegate das; await[
+        #region]
+    }
+}
+global;
